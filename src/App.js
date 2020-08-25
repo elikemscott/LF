9a4ae98d86481931ce090543f45afc72478808be
elikemscott <kelvynellyson@yahoo.com>
2020-08-25 18:14:07 +0000

    landing page v2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
-import Nav from './components/Nav';
-import Practice_areas from './components/Practice_areas';
 import Parallax from './components/Parallax';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
       <Navbar/>
       <Carousel/>
       <Parallax/>
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,35 +1,107 @@
-import React from 'react'
+import React from "react";
 
 function Carousel() {
-    return (
-        <div>
-            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
-        alt="First slide"/>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(16).jpg"
-        alt="Second slide"/>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(17).jpg"
-        alt="Third slide"/>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-  
-</div>
+  return (
+    <div>
+      <div
+        id="carousel-example-2"
+        class="carousel slide carousel-fade"
+        data-ride="carousel"
+      >
+        <ol class="carousel-indicators">
+          <li
+            data-target="#carousel-example-2"
+            data-slide-to="0"
+            class="active"
+          ></li>
+          <li data-target="#carousel-example-2" data-slide-to="1"></li>
+          <li data-target="#carousel-example-2" data-slide-to="2"></li>
+        </ol>
+
+        <div class="carousel-inner" role="listbox">
+          <div class="carousel-item active">
+            <div class="view">
+              <img
+                class="d-block w-100 "
+                src="https://images.unsplash.com/photo-1526615735835-530c611a3d8a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="First slide"
+                height="630px"
+              />
+              <div class="mask rgba-black-slight"></div>
+            </div>
+            <div class="carousel-caption">
+              <h3 class="h3-responsive"></h3>
+              <p>
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                blanditiis praesentium voluptatum deleniti atque corrupti quos
+                dolores et quas molestias excepturi sint occaecati cupiditate
+                non provident, similique sunt in culpa qui officia deserunt
+                mollitia animi, id est laborum et dolorum fuga. Et harum quidem{" "}
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="view">
+              <img
+                class="d-block w-100"
+                src="https://images.unsplash.com/photo-1505664194779-8beaceb93744?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="Second slide"
+                height="630px"
+              />
+              <div class="mask rgba-black-slight"></div>
+            </div>
+            <div class="carousel-caption">
+              <h3 class="h3-responsive"></h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Eligendi non quis exercitationem culpa nesciunt nihil aut
+                nostrum explicabo reprehenderit{" "}
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="view">
+              <img
+                class="d-block w-100"
+                src="https://images.unsplash.com/photo-1436450412740-6b988f486c6b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="Third slide"
+                height="630px"
+              />
+              <div class="mask rgba-black-slight"></div>
+            </div>
+            <div class="carousel-caption">
+              <h3 class="h3-responsive"></h3>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi architecto beatae
+                vitae dicta sunt explicabo.{" "}
+              </p>
+            </div>
+          </div>
         </div>
-    )
+
+        <a
+          class="carousel-control-prev"
+          href="#carousel-example-2"
+          role="button"
+          data-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a
+          class="carousel-control-next"
+          href="#carousel-example-2"
+          role="button"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Carousel;
